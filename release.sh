d4af00ea507f4ddae427d1e03bbe4bd2ceb3e207
Mitchel Kuijpers <mitchelkuijpers@gmail.com>
2012-09-24 10:55:47 +0200

    added robocode-core

--- release.sh
+++ release.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 mvn deploy:deploy-file -DgroupId=net.sf.robocode \
-  -DartifactId=robocode \
-  -Dversion=1.7.4.2 \
-  -Dpackaging=jar \
-  -Dfile=robocode.jar \
-  -Durl=file://m2 \
-  -Djavadoc=javadoc.jar
+  	-DartifactId=robocode \
+  	-Dversion=1.7.4.2 \
+  	-Dpackaging=jar \
+  	-Dfile=robocode.jar \
+  	-Durl=file://m2 \
+  	-Djavadoc=javadoc.jar
+
+mvn deploy:deploy-file -DgroupId=net.sf.robocode \
+  	-DartifactId=robocode-core \
+  	-Dversion=1.7.4.2 \
+  	-Dpackaging=jar \
+  	-Dfile=robocode.core-1.7.4.2.jar \
+  	-Durl=file://m2 \
+  	-Djavadoc=javadoc.jar
